1f6b8683215e50ae6bcfad170c800bc3b3e96b3c
Emiliano <eviernes52@gmail.com>
2022-06-01 00:00:50 -0700

    Added Final Pages and composer.json

---
+++ Index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="main.css" type="text/css" media="screen" />
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon_package_v0.16/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon_package_v0.16/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon_package_v0.16/favicon-16x16.png">
+	<link rel="manifest" href="/favicon_package_v0.16/site.webmanifest">
+	<link rel="mask-icon" href="/favicon_package_v0.16/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
+</head>
+
+<body>
+	<nav>
+		<div class="logo">
+			<a href="index.html">
+				<img class="logo" src="/img/Mi California Promo Logo.png" alt="nav-logo"></img>
+		</div>
+		<ul class=nav-links>
+			<li><a href="specials.html">Specials</a></li>
+			<li><a href="about us.html">About</a></li>
+			<li><a href="contact.html">Contact</a></li>
+			<li><a href="menu.html">Menu</a></li>
+			<li><a href="reserve.html">Reserve</a></li>
+		</ul>
+
+		<button class="reservation-button" onclick="location.href='Reservations.html'">Login</button>
+
+		<div class="burger">
+			<div class="line1"></div>
+			<div class="line2"></div>
+			<div class="line3"></div>
+		</div>
+	</nav>
+	<script src="burger.js"></script>
+</body>
+
+</html>
